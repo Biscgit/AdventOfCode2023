0abd7659a09480cab010f9958181f269277dfc2d
David Horvát <horvatda@proton.me>
2023-12-09 14:29:23 +0100

    new default main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
 fn main() {
-    println!("Hello, world!");
+    let file_path = "src/DayX/input.txt";
+    let file = File::open(file_path).unwrap();
+
+    let reader = BufReader::new(file);
+
+    for line in reader.lines() {
+        let line = line.unwrap();
+    }
 }
